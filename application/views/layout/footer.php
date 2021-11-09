f90f0e3ed55acc08d0ab0a59cc1b4121a0a59115
akhmad aziz mario <12201084@bsi.ac.id>
2021-11-09 09:05:53 +0700

    update terbaru

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,19 +1,19 @@
 <!-- Bootstrap core JavaScript-->
-<script src="http://localhost/phpmvc/assets/vendor/jquery/jquery.min.js"></script>
-<script src="http://localhost/phpmvc/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="http://localhost/latihansppwebprogram/assets/vendor/jquery/jquery.min.js"></script>
+<script src="http://localhost/latihansppwebprogram/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="http://localhost/phpmvc/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="http://localhost/latihansppwebprogram/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="http://localhost/phpmvc/assets/js/sb-admin-2.min.js"></script>
+<script src="http://localhost/latihansppwebprogram/assets/js/sb-admin-2.min.js"></script>
 
 <!-- Page level plugins -->
-<script src="http://localhost/phpmvc/assets/vendor/chart.js/Chart.min.js"></script>
+<script src="http://localhost/latihansppwebprogram/assets/vendor/chart.js/Chart.min.js"></script>
 
 <!-- Page level custom scripts -->
-<script src="http://localhost/phpmvc/assets/js/demo/chart-area-demo.js"></script>
-<script src="http://localhost/phpmvc/assets/js/demo/chart-pie-demo.js"></script>
+<script src="http://localhost/latihansppwebprogram/assets/js/demo/chart-area-demo.js"></script>
+<script src="http://localhost/latihansppwebprogram/assets/js/demo/chart-pie-demo.js"></script>
 
 </body>
 <!----footer-------->
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -13,11 +13,11 @@
     <link rel="shortcut icon" href="https://i.pinimg.com/originals/c1/94/32/c194329bae6d1f4fbe3890136d9fdb5f.png">
 
     <!-- Custom fonts for this template-->
-    <link href="http://localhost/phpmvc/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="http://localhost/latihansppwebprogram/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="http://localhost/phpmvc/assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="http://localhost/latihansppwebprogram/assets/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
